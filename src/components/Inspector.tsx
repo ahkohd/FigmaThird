0889cf437d86233905ce1fb72c424be5474f8d16
Victor Aremu <victor.olorunbunmi@gmail.com>
2019-11-15 19:58:29 +0100

    Code clean up and documentation

--- src/components/Inspector.tsx
+++ src/components/Inspector.tsx
@@ -7,9 +7,11 @@ import Container from "./TreeBread/TreeBreadCustomContainer";
 import Toggle from "./TreeBread/TreeBreadCustomToggle";
 
 import useViewportItemSelected from "../hooks/viewportItemSelected";
+import Tree from "../utils/Tree";
 
 export default function Inspector() {
     const { state }: any = React.useContext(AppContext);
+
     const [data, setData] = React.useState(state.sceneTree);
     const [cursor, setCursor]: any = React.useState(false);
 
@@ -20,8 +22,15 @@ export default function Inspector() {
         setData(state.sceneTree);
     }, [state.sceneTree]);
 
-    const onToggle = (node, toggled) => {
-        // return;
+    /**
+     * Toggle a node toggle propert and set the node active.
+     *  Also If the toggler is a `togglebutton` it reveales its child
+     * nodes
+     * @param {Tree} node to set active.
+     * @param {boolean} toggled `toggled` value of the node.
+     */
+
+    const onToggle = (node: Tree, toggled: boolean) => {
         if (cursor) {
             cursor.active = false;
             traverseNode(data.children, childNode => {
@@ -38,6 +47,11 @@ export default function Inspector() {
         setCursor(node);
         setData(Object.assign({}, data));
     };
+
+    /**
+     * Builds up Inspector tree UI
+     * @param data Tree data that was populated based on objects in the Three.JS scene which resides in the viewport.
+     */
 
     const buildTree = data => {
         return (
