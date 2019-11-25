245c611106eb6d9ae130ec52a5fa94696b55b730
Victor Aremu <victor.olorunbunmi@gmail.com>
2019-11-25 13:11:12 +0100

    Updates

--- src/components/FootPanel/Keybindings.tsx
+++ src/components/FootPanel/Keybindings.tsx
@@ -1,10 +1,13 @@
 import * as React from "react";
-import { H2 } from "./Styled";
+import { H2, KBD, FlexSpaceCenter, Ktext } from "./Styled";
 
 export default function Keybindings() {
     return (
         <>
-            <H2>Keybindings</H2>
+            <H2>Transform keyboard Shortcuts</H2>
+            <FlexSpaceCenter>
+                <KBD>W</KBD>
+            </FlexSpaceCenter>
         </>
     );
 }
--- src/components/FootPanel/Styled.tsx
+++ src/components/FootPanel/Styled.tsx
@@ -4,3 +4,22 @@ export const H2 = styled.h2`
     font: caption;
     font-size: 14px;
 `;
+
+export const KBD = styled.kbd`
+    background: #00aced;
+    border-radius: 2px;
+    padding: 3px 5px;
+    color: black;
+    font: caption;
+    cursor: default;
+`;
+
+export const FlexSpaceCenter = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+export const KText = styled.div`
+    color: #aaa;
+`;
